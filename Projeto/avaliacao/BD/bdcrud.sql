5d77373182cec3ab9f761b00d85a72a945fe1d31
rod9000 <rodrigo1048@hotmail.com>
2021-05-14 00:59:10 -0300

    Update bdcrud.sql

--- Projeto/avaliacao/BD/bdcrud.sql
+++ Projeto/avaliacao/BD/bdcrud.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Maio-2021 às 22:11
--- Versão do servidor: 10.4.18-MariaDB
--- versão do PHP: 7.3.28
+-- Tempo de geração: 14-Maio-2021 às 05:58
+-- Versão do servidor: 10.4.13-MariaDB
+-- versão do PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -56,7 +56,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_login`, `password_hash`, `user_full_name`, `user_email`) VALUES
-(1, 'admin', '$2y$10$9qEYBLlOSnhyuPqPeks.demJxLHU/mCq6szSIYbBQPu9.sNFiBxIC', 'admin', 'admin@domain.com'),
+(1, 'admin', '$2y$10$0IaXa67wfzZKUMuea/PJhOI8GaKO6JbUkcYiGTGv3j6URTd9zBJZm', 'admin', 'admin@domain.com'),
 (3, 'admin1', '$2y$10$kPAGwPg049lGkP1ctLnUBuAjDA8oPDEsS6QkARMb20dauuaDRGN.K', 'admin1', 'admin1');
 
 -- --------------------------------------------------------
@@ -67,12 +67,22 @@ INSERT INTO `users` (`user_id`, `user_login`, `password_hash`, `user_full_name`,
 
 CREATE TABLE `viagens` (
   `id_viagens` int(11) NOT NULL,
-  `local` varchar(100) NOT NULL,
-  `data` date NOT NULL,
-  `servico` varchar(100) NOT NULL,
-  `funcionario` varchar(50) NOT NULL,
-  `valor` decimal(10,0) NOT NULL
+  `vg_destino` varchar(100) NOT NULL,
+  `vg_dsaida` date NOT NULL,
+  `vg_dretorno` date NOT NULL,
+  `vg_servico` varchar(100) NOT NULL,
+  `vg_funcionario` varchar(50) NOT NULL,
+  `vg_valorIn` decimal(10,0) NOT NULL,
+  `vg_realizada` varchar(20) NOT NULL,
+  `vg_motivo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `viagens`
+--
+
+INSERT INTO `viagens` (`id_viagens`, `vg_destino`, `vg_dsaida`, `vg_dretorno`, `vg_servico`, `vg_funcionario`, `vg_valorIn`, `vg_realizada`, `vg_motivo`) VALUES
+(1, 'teste', '0000-00-00', '3111-12-12', 'teste', 'teste', '12', 'teste', 'teste');
 
 --
 -- Índices para tabelas despejadas
@@ -116,7 +126,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `viagens`
 --
 ALTER TABLE `viagens`
-  MODIFY `id_viagens` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_viagens` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
